d4b87e1561acfe5b3ae70c79c57242349e2fedad
Tom Denley <t.denley@strategic-blue.com>
2021-12-28 19:17:29 +0000

    only the genclass requires qualified types

--- src/main/clojure/com/scarytom/tbs/java_interop.clj
+++ src/main/clojure/com/scarytom/tbs/java_interop.clj
@@ -1,5 +1,6 @@
 (ns com.scarytom.tbs.java-interop
-  (:require [com.scarytom.tbs.beat-box :as bb]))
+  (:require [com.scarytom.tbs.beat-box :as bb])
+  (:import [com.scarytom.tbs BeatBox]))
 
 (gen-class
   :name com.scarytom.tbs.JavaInterop
@@ -7,7 +8,7 @@
   :methods [#^{:static true} [makeBeatBox [] com.scarytom.tbs.BeatBox]])
 
 (defrecord JavaBeatBox []
-  com.scarytom.tbs.BeatBox
+  BeatBox
   (kick [_this duration]
     (bb/kick duration)))
 
